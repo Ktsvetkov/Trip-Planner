148ceb5ec2f57bca3367bdad3a6416ed7a528611
David Heavern <dheavern@gmail.com>
2014-06-05 00:02:53 -0400

    Added login methods

--- src/main/java/edu/gatech/cs2340/trip/model/LoginManager.java
+++ src/main/java/edu/gatech/cs2340/trip/model/LoginManager.java
@@ -1,7 +1,25 @@
 package edu.gatech.cs2340.trip.model;
 
+import javax.security.auth.login.AccountException;
+import javax.security.auth.login.AccountNotFoundException;
+
 /**
  * Created by dheavern on 6/4/14.
  */
 public class LoginManager {
+    private static AccountDAO database = new SqlliteAccountDAO();
+
+    public static Account attemptLogin(String accountName, String passwordPlaintext)
+            throws AccountException, AccountNotFoundException {
+        Account matchedAccount;
+        matchedAccount = database.getAccount(accountName);
+        try {
+            if (PasswordHash.validatePassword(passwordPlaintext, matchedAccount.getPasswordHash())) {
+                return matchedAccount;
+            }
+        } catch (Exception e) {
+
+        }
+        return null;
+    }
 }
