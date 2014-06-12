0f1eae23d81089474ba451988f407b6eac84a502
David Heavern <heavernd@gatech.edu>
2014-06-11 22:24:17 -0400

    Better error handling

--- src/main/java/edu/gatech/cs2340/trip/model/UpdateAccountManger.java
+++ src/main/java/edu/gatech/cs2340/trip/model/UpdateAccountManger.java
@@ -3,15 +3,17 @@ package edu.gatech.cs2340.trip.model;
 import edu.gatech.cs2340.trip.util.InputValidator;
 import edu.gatech.cs2340.trip.util.PasswordHash;
 
+import javax.security.auth.login.AccountException;
+
 /**
  * Created by dheavern on 6/4/14.
  */
 public class UpdateAccountManger {
     private static AccountDAO database = new SqlliteAccountDAO();
 
-    public static boolean updateEmail(Account account,String newEmail) {
+    public void updateEmail(Account account,String newEmail) throws AccountException {
         if (!InputValidator.isValidEmail(newEmail)) {
-            return false;
+            throw new AccountException("The supplied email is invalid");
         }
         String oldEmail = account.getEmail();
         account.setEmail(newEmail);
@@ -19,42 +21,42 @@ public class UpdateAccountManger {
             database.updateAccount(account);
         } catch (Exception e) {
             account.setEmail(oldEmail);
-            return false;
+            throw new AccountException("There was an error updating your email.");
         }
-        return true;
     }
 
-    public static boolean updatePassword(Account account,
+    public static void updatePassword(Account account,
                                          String oldPassword,
                                          String newPassword,
-                                         String confirmPassword) {
+                                         String confirmPassword) throws AccountException {
         String oldPasswordHash = account.getPasswordHash();
         try {
             if (!PasswordHash.validatePassword(oldPassword, oldPasswordHash)) {
-                return false;
+                throw new AccountException("Passwords must be 3-15 alpha-numeric characters");
             }
         } catch (Exception e) {
-            return false;
+            throw new AccountException("There was an internal error");
         }
         if (!newPassword.equals(oldPassword)) {
-            return false;
+            throw new AccountException("The password you entered is not correct");
+        }
+        if (!newPassword.equals(confirmPassword)) {
+            throw new AccountException("Your passwords do not match");
         }
         account.setPassword(newPassword);
         try {
             database.updateAccount(account);
         } catch (Exception e) {
             account.setPassword(oldPassword);
-            return false;
+            throw new AccountException("There was an error changing your password.");
         }
-        return true;
     }
 
-    public static boolean saveTripChanges(Account account) {
+    public static void saveTripChanges(Account account) {
         try {
             database.updateAccount(account);
         } catch (Exception e) {
-            return false;
+
         }
-        return true;
     }
 }
